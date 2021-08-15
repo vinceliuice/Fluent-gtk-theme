045ad807e97a114e4ddb1c3c0b134afe0672220e
Vince <vinceliuice@hotmail.com>
2021-08-15 11:57:30 +0800

    update

--- src/wallpaper/install-gnome-backgrounds.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-REPO_DIR="$(cd "$(dirname "$0")" && pwd)"
-BACKGROUND_DIR="/usr/share/backgrounds"
-PROPERTIES_DIR="/usr/share/gnome-background-properties"
-
-cp -r $REPO_DIR/fluent $BACKGROUND_DIR
-cp -r $REPO_DIR/gnome-background-properties/fluent.xml $PROPERTIES_DIR
