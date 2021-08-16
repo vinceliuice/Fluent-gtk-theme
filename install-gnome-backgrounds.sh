13d6fb9baf9b83c265d5c65ac19976271a10e4b6
Vince <vinceliuice@hotmail.com>
2021-08-16 09:02:31 +0800

    Update install-gnome-backgrounds.sh

--- install-gnome-backgrounds.sh
+++ install-gnome-backgrounds.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+ROOT_UID=0
+
 REPO_DIR="$(cd "$(dirname "$0")" && pwd)"
 BACKGROUND_DIR="/usr/share/backgrounds"
 PROPERTIES_DIR="/usr/share/gnome-background-properties"
+
+if [[ $UID -ne $ROOT_UID ]];  then
+  echo -e "\n ERROR: Need root access! please run this script with sudo. \n"
+  exit 1
+fi
 
 for theme in '' '-mountain' '-building'; do
   echo -e "\n Install fluent${theme}... \n"
